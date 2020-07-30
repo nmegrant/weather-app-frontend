9070d64f8b712b422fc4996fad940a5c6edf2d50
nmegrant <nme.grant@gmail.com>
2020-07-30 14:48:31 +0200

    Set up store and root reducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import MainPage from "./pages/MainPage";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather app!</h1>
+      <Switch>
+        <Route exact path="/" component={MainPage} />
+      </Switch>
     </div>
   );
 }
