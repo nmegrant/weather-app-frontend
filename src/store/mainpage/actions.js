8b06e0dacb630c34520a9b646c00a8666bf38392
nmegrant <nme.grant@gmail.com>
2020-07-30 16:22:26 +0200

    Create actions and selector for mainpage weather

--- src/store/mainpage/actions.js
+++ src/store/mainpage/actions.js
@@ -1,9 +1,32 @@
 import axios from "axios";
 
+export function mainPageWeatherFetched(cities) {
+  return {
+    type: "MAINPAGE_WEATHER_FETCHED",
+    payload: cities,
+  };
+}
+
 export function fetchMainPageWeatherThunkCreator() {
   return async function mainPageWeather(dispatch, getState) {
     try {
-      const response = await axios.get(``);
+      const cities = [
+        "Wellington,nz",
+        "Halifax,ca",
+        "Amsterdam,nl",
+        "Tokyo,jp",
+        "Buenos Aires,ar",
+        "Sydney,au",
+      ];
+      const response = await Promise.all(
+        cities.map(async (city) => {
+          return await axios.get(
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${process.env.REACT_APP_API_KEY}`
+          );
+        })
+      );
+      const responseData = response.map((response) => response.data);
+      dispatch(responseData);
     } catch (error) {
       console.log(error);
     }
--- src/store/mainpage/reducer.js
+++ src/store/mainpage/reducer.js
@@ -2,6 +2,8 @@ const initialState = [];
 
 export default function mainpageReducer(state = initialState, action) {
   switch (action.type) {
+    case "MAINPAGE_WEATHER_FETCHED":
+      return [...action.payload];
     default:
       return state;
   }
