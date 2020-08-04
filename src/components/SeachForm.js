a1009c56a6851ce85c46faf2ce7efc74a53f96f2
nmegrant <nme.grant@gmail.com>
2020-08-04 23:36:41 +0200

    Create controlled form for city search, fetch city from API

--- src/components/SeachForm.js
+++ src/components/SeachForm.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export default function SearchForm() {
+  const [search, setSearch] = useState("");
+
+  async function submitCity(event) {
+    event.preventDefault();
+    const queryParams = encodeURIComponent(search);
+    const response = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?q=${queryParams}&APPID=${process.env.REACT_APP_API_KEY}`
+    );
+    setSearch("");
+    console.log(response.data);
+  }
+
   return (
     <div>
-      <p>search</p>
+      <form>
+        <label>Search for a city</label>{" "}
+        <input
+          type="text"
+          value={search}
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button style={{ marginLeft: "5px" }} onClick={submitCity}>
+          search
+        </button>
+      </form>
     </div>
   );
 }
