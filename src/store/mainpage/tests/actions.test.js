7bdc2438ee6e31ad97b3abade3adab308bcc028d
nmegrant <nme.grant@gmail.com>
2020-08-05 17:56:13 +0200

    Add tests for mainpage actions

---
+++ src/store/mainpage/tests/actions.test.js
@@ -0,0 +1,33 @@
+import { mainPageWeatherFetched } from "../actions";
+
+describe("#mainPageWeatherFetched", () => {
+  describe("if given an array of city objects", () => {
+    test("should return an action object", () => {
+      const cities = [{}, {}];
+      const expected = {
+        type: "MAINPAGE_WEATHER_FETCHED",
+        payload: cities,
+      };
+      expect(mainPageWeatherFetched(cities)).toEqual(expected);
+    });
+    test("action.payload should have same length as given arguement", () => {
+      const cities = [{}, {}];
+      const expected = {
+        type: "MAINPAGE_WEATHER_FETCHED",
+        payload: cities,
+      };
+      expect(mainPageWeatherFetched(cities).payload).toHaveLength(
+        cities.length
+      );
+    });
+  });
+  describe("if given a null argument", () => {
+    test("action.payload should be null", () => {
+      const expected = {
+        type: "MAINPAGE_WEATHER_FETCHED",
+        payload: null,
+      };
+      expect(mainPageWeatherFetched(null)).toEqual(expected);
+    });
+  });
+});
