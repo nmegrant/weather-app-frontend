5ce51bdcdbeea733aa5ec9fd93384a4e57abf8b2
nmegrant <nme.grant@gmail.com>
2020-08-10 17:26:08 +0200

    Create login, signup pages and navbar component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
-import MainPage from "./pages/MainPage";
-
 import "./App.css";
+import MainPage from "./pages/MainPage";
 import CityPage from "./pages/CityPage";
+import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Switch>
         <Route exact path="/" component={MainPage} />
         <Route path="/:cityId" component={CityPage} />
+        <Route path="/login" component={Login} />
+        <Route path="./signup" component={SignUp} />
       </Switch>
     </div>
   );
