cc758a1477b1ba262f15faade5ae1cf75f01f135
nmegrant <nme.grant@gmail.com>
2020-08-10 22:39:54 +0200

    Set up form for user sign up

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,12 +2,38 @@ import React, { useState } from "react";
 
 export default function SignUp() {
   const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [location, setLocation] = useState("");
+
+  function submitSignup(event) {
+    event.preventDefault();
+    console.log(name, location, email, password);
+  }
+
   return (
     <div>
       <h1>Sign up!</h1>
+      <form>
+        <label>Name</label>
+        <input value={name} onChange={(event) => setName(event.target.value)} />
+        <label>Location</label>
+        <input
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+        />
+        <label>Email</label>
+        <input
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <label>Password</label>
+        <input
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button onClick={submitSignup}>Sign up</button>
+      </form>
     </div>
   );
 }
