2118c6c986366ef3e263f5151896dae985c072cc
nmegrant <nme.grant@gmail.com>
2020-08-24 18:07:49 +0200

    Use lifting state and callback props to change favorite state

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -1,9 +1,17 @@
 import React from "react";
 
-export default function Favourites() {
+export default function Favourites(props) {
+  function clickButton(event) {
+    event.preventDefault();
+    props.changeFavourite();
+  }
+
   return (
     <div>
-      <h5>Favourite: ♡</h5>
+      <h5>
+        Favourite:{" "}
+        <button onClick={clickButton}>{props.favourite ? "❤️" : "♡"}</button>
+      </h5>
     </div>
   );
 }
--- src/pages/CityPage.js
+++ src/pages/CityPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -12,14 +12,20 @@ export default function CityPage() {
   const parameters = useParams();
   const dispatch = useDispatch();
   const weather = useSelector(selectcityWeather());
+  const [favourite, setFavourite] = useState(false);
 
   useEffect(() => {
     dispatch(fetchCityWeatherThunkCreator(parameters.cityId));
   }, [parameters.cityId, dispatch]);
 
+  function changeFavourite() {
+    setFavourite(!favourite);
+  }
+
   return (
     <div>
       <h1>{weather.name}</h1>
+      <Favourites changeFavourite={changeFavourite} favourite={favourite} />
       <CityWeather
         main={weather.main}
         weather={weather.weather}
