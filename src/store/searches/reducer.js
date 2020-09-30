0456b7e1ef89a545318b926e0e9ce019c7000671
nmegrant <nme.grant@gmail.com>
2020-09-30 14:57:26 +0200

    Update root reducer

--- src/store/searches/reducer.js
+++ src/store/searches/reducer.js
@@ -4,5 +4,7 @@ export default function searchReducer(state = initialState, action) {
   switch (action.type) {
     case "FETCH_SEARCH_DATA":
       return [...action.payload];
+    default:
+      return state;
   }
 }
