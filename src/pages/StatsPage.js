dac422667a19d2f1cf35831a050dc9d0ac0ee97e
nmegrant <nme.grant@gmail.com>
2020-09-30 15:03:00 +0200

    Select searches from store

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSearchesThunkCreator } from "../store/searches/actions";
+import { selectSearches } from "../store/searches/selectors";
 
 export default function StatsPage() {
   const dispatch = useDispatch();
+  const searches = useSelector(selectSearches());
+
+  console.log(searches);
 
   useEffect(() => {
     dispatch(fetchSearchesThunkCreator());
