3ab5fe1415814813237e44ea1757c90afbd0434c
nmegrant <nme.grant@gmail.com>
2020-09-30 15:10:48 +0200

    Install d3 library

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSearchesThunkCreator } from "../store/searches/actions";
 import { selectSearches } from "../store/searches/selectors";
+import * as d3 from d3
 
 export default function StatsPage() {
   const dispatch = useDispatch();
