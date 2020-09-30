a857e5c0a7e2515f76691527c5454bc9fc812d51
nmegrant <nme.grant@gmail.com>
2020-09-30 15:13:53 +0200

    Fix security concerns

--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSearchesThunkCreator } from "../store/searches/actions";
 import { selectSearches } from "../store/searches/selectors";
-import * as d3 from d3
+import * as d3 from "d3";
 
 export default function StatsPage() {
   const dispatch = useDispatch();
   const searches = useSelector(selectSearches());
-
-  console.log(searches);
 
   useEffect(() => {
     dispatch(fetchSearchesThunkCreator());
